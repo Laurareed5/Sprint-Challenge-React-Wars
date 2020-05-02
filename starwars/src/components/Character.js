184f616927dfcba7d522ae05108746dbb5a2634c
Laura reed <pilesoflaura@gmail.com>
2020-05-02 11:56:36 -0600

    If run, this code will have character card and styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,17 +1,15 @@
 // Write your Character component here
-import React from "react";
+import React from 'react';
 import styled from "styled-components";
 
-
-const Card = styled.div`
-  background: #99f3eb;
+const CharacterCard = styled.div`
+  background: #e89242ff;
   color: black;
-  width: 200px;
-  max-height: 1300px;
+  width: 300px;
   display: flex;
   flex-direction: column;
-
-  justify-content: space-between;
+  align-items: center;
+  margin-left: 650px;
   margin-bottom: 20px;
   box-shadow: 2px 2px 2px grey;
 `;
@@ -19,21 +17,18 @@ const Card = styled.div`
 const CharacterImg = styled.img`
   width: 200px;
   height: 200px;
-  object-fit: scale;
-  flex-shrink: 2;
 `;
 
-
 const Character = (props) => {
-  return (
-    <Card>
-      <h2>{props.name}</h2>
-      <CharacterImg top width="10%" src={props.img} alt="Characters" />
-      <p>{props.gender}</p>
-      <p>{props.status}</p>
-      <p>{props.species}</p>
-    </Card>
-  );
-};
+    return (
+        <CharacterCard>
+            <h2>{props.name}</h2>
+            <CharacterImg top width="10%" src={props.img} alt="Characters" />
+            <p>{props.species}</p>
+            <p>{props.gender}</p>
+            <p>{props.status}</p>
+        </CharacterCard>
+    )
+}
 
-export default Character; 
+export default Character;
